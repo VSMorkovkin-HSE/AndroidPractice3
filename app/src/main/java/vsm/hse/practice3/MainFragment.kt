f270ea48b38b55b12ee65c77549c45eeb1951eec
vsmorkovkin <thegramanhuth@mail.ru>
2022-11-25 19:14:43 +0300

    make buttons show, add, delete navigate to corresponding fragments

--- app/src/main/java/vsm/hse/practice3/MainFragment.kt
+++ app/src/main/java/vsm/hse/practice3/MainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class MainFragment : Fragment() {
 
@@ -14,6 +16,23 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_main, container, false)
+
+
+        val showButton = view.findViewById<Button>(R.id.show)
+        showButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_mainFragment_to_showFragment)
+        }
+
+        val addButton = view.findViewById<Button>(R.id.add)
+        addButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_mainFragment_to_addFragment)
+        }
+
+        val deleteButton = view.findViewById<Button>(R.id.delete)
+        deleteButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_mainFragment_to_deleteFragment)
+        }
+
         return view
     }
 }
